3e9bd3b40a0932a170874268cea5d8a6c0676c7a
Marius Banea <marius@w-d-s.co.uk>
2021-09-14 00:21:17 +0100

    updated the error message

--- js/api-calls.js
+++ js/api-calls.js
@@ -5,83 +5,73 @@ $(document).ready(function () {
 
 
     //step 1 get the input from the user
-    $("#searchBar").submit(function (event) {
+    $(".is-this-number-prime").submit(function (event) {
 
         //force JavaScript to handle the submission
         event.preventDefault();
 
         //get the value from the input box
-        let userSearch = $("#text-box").val();
+        let is_this_number_prime_apiKey = $("#is-this-number-prime-apiKey").val();
+        let is_this_number_prime_check_number = $("#is-this-number-prime-number-check-number").val();
+        let is_this_number_prime_include_explanations = $("#is-this-number-prime-include-explanations").val();
+        let is_this_number_prime_include_prime_types_list = $("#is-this-number-prime-include-prime-types-list").val();
+        let is_this_number_prime_language = $("#is-this-number-prime-language").val();
 
-
+        // console.log(is_this_number_prime_apiKey, is_this_number_prime_check_number, is_this_number_prime_include_explanations, is_this_number_prime_include_prime_types_list, is_this_number_prime_language)
 
         //use the value to call the getResults function defined below
-        DataFromUser(userSearch);
+        is_this_number_prime_api_call(is_this_number_prime_apiKey, is_this_number_prime_check_number, is_this_number_prime_include_explanations, is_this_number_prime_include_prime_types_list, is_this_number_prime_language);
     });
 
     //step2 using the input from the user make the API call to get the JSON response
 
-    function DataFromUser(SearchArtist) {
-        $.getJSON("https://api.songkick.com/api/3.0/search/artists.json?query=" + SearchArtist + "&apikey=ibjKuqIpOmtRftG3&jsoncallback=?",
-            function (receiveData) {
-                // data is JSON response object
-                console.log(receiveData.resultsPage.totalEntries);
-                //if there are no results it will empty the list
-                if (receiveData.resultsPage.totalEntries == 0) {
-                    swal({
-                        title: "Bummer!",
-                        text: "We can't find " + SearchArtist + "!",
-                        icon: "warning",
-                    });
-                } else {
-                    //else there are results, call the display search results
-                    $.getJSON("https://api.songkick.com/api/3.0/artists/" + receiveData.resultsPage.results.artist[0].id + "/calendar.json?apikey=ibjKuqIpOmtRftG3&jsoncallback=?",
-                        function (receiveData) {
-                            // data is JSON response object
-                            console.log(receiveData.resultsPage.totalEntries);
-                            //if there are no results it will empty the list
-                            if (receiveData.resultsPage.totalEntries == 0) {
-                                swal({
-                                    title: "Aw man!",
-                                    text: "Looks like " + SearchArtist + " isn't touring at this time!",
-                                    icon: "warning",
-                                });
-                            }
-                            //else there are results, call the display search results
-                            displayResults(receiveData.resultsPage.results.event);
-                        })
-                }
-            })
-        $('#searchBar').prop('hidden', false);
+    function is_this_number_prime_api_call(is_this_number_prime_apiKey, is_this_number_prime_check_number, is_this_number_prime_include_explanations, is_this_number_prime_include_prime_types_list, is_this_number_prime_language) {
+        let is_this_number_prime_api_url = `http://api.prime-numbers.io/is-this-number-prime.php?key=${is_this_number_prime_apiKey}&number=${is_this_number_prime_check_number}&include_explanations=${is_this_number_prime_include_explanations}&include_prime_types_list=${is_this_number_prime_include_prime_types_list}&language=${is_this_number_prime_language}`
+
+        console.log(is_this_number_prime_api_url)
+
+        $.ajax({
+            url: is_this_number_prime_api_url,
+            type: 'get',
+            cache: false,
+            success: function (dataReceived) {
+                console.log('success!');
+                console.log(dataReceived);
+            },
+            error: function(XMLHttpRequest, textStatus, errorThrown) {
+                console.log("Status: ", textStatus); 
+                console.log("Error: ", errorThrown);
+                console.log("XMLHttpRequest: ", XMLHttpRequest.responseJSON.error);
+                displayError(XMLHttpRequest.responseJSON.error, "error-is-this-number-prime")
+            }
+        });
     };
 })
 
 //step3 using the JSON response, populate the relevant part of your HTML with the variable inside the JSON
 
-function displayResults(artistArray) {
+function is_this_number_prime_api_results(dataReceived) {
 
-    console.log(artistArray);
-
-
+    console.log(dataReceived);
 
     //create an empty variable to store a new list item for each result
     let buildHtmlResults = "";
 
-    $.each(artistArray, function (artistArrayKey, artistArrayValue) {
+    $.each(dataReceived, function (dataReceivedKey, dataReceivedValue) {
         buildHtmlResults += "<li>";
         buildHtmlResults += "<div class='event-display-details'>";
-        buildHtmlResults += "<div class='event-display-name' >" + artistArrayValue.displayName + "</div>"
-        buildHtmlResults += "<div class='event-details-start-date' >" + artistArrayValue.start.date + "</div>";
+        buildHtmlResults += "<div class='event-display-name' >" + dataReceivedValue.displayName + "</div>"
+        buildHtmlResults += "<div class='event-details-start-date' >" + dataReceivedValue.start.date + "</div>";
 
-        buildHtmlResults += "<div class='event-details-city' >" + artistArrayValue.location.city + "</div><br>";
+        buildHtmlResults += "<div class='event-details-city' >" + dataReceivedValue.location.city + "</div><br>";
 
-        buildHtmlResults += "<a href='" + artistArrayValue.uri + "' class='event-details-button' target='_blank'>More Info</a>";
+        buildHtmlResults += "<a href='" + dataReceivedValue.uri + "' class='event-details-button' target='_blank'>More Info</a>";
         buildHtmlResults += "</div>";
         buildHtmlResults += "</div>";
         buildHtmlResults += "<div class='event-details-venue' >";
 
-        if (artistArrayValue.venue.displayName != null || artistArrayValue.location.city != null) {
-            buildHtmlResults += "<iframe width='100%' height='150px'frameborder='0' style='border:0; clear: both; margin:10px;' src='https://www.google.com/maps/embed/v1/place?key=AIzaSyBdNRsY4zEYnRfcQ0_ZVVd370D7yuApzhI&q=" + artistArrayValue.venue.displayName + "," + artistArrayValue.location.city + "&maptype=roadmap' allowfullscreen></iframe>";
+        if (dataReceivedValue.venue.displayName != null || dataReceivedValue.location.city != null) {
+            buildHtmlResults += "<iframe width='100%' height='150px'frameborder='0' style='border:0; clear: both; margin:10px;' src='https://www.google.com/maps/embed/v1/place?key=AIzaSyBdNRsY4zEYnRfcQ0_ZVVd370D7yuApzhI&q=" + dataReceivedValue.venue.displayName + "," + dataReceivedValue.location.city + "&maptype=roadmap' allowfullscreen></iframe>";
         }
         buildHtmlResults += "</div>";
         buildHtmlResults += "<div class='event-display-required-image' >";
@@ -93,4 +83,17 @@ function displayResults(artistArray) {
 
     //use the html output to show it in the index.html
     $('#search-results ul').html(buildHtmlResults);
+};
+
+function displayError(errorText, containerClass) {
+
+    console.log(errorText);
+
+    let buildHtmlError = `<button type="button" class="close" data-dismiss="alert" aria-label="Close">`;
+    buildHtmlError += `<span aria-hidden="true">×</span>`;
+    buildHtmlError += `</button>`;
+    buildHtmlError += `<strong>${errorText}</strong>`;
+
+    $(`.${containerClass}`).html(buildHtmlError);
+    $(`.${containerClass}`).show();
 };
